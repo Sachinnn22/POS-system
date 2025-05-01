99ecf0604323d844e07c75c1cee0d571fe42e99a
Sachinnn22 <sachinduchirau2@gmail.com>
2025-05-01 14:01:56 +0530

    update db and customer model

---
+++ model/customerModel.js
@@ -0,0 +1,9 @@
+export default class customerModel {
+  constructor(cusId,cusName,age,contact,address) {
+      this.cusId = cusId;
+      this.cusName = cusName;
+      this.age = age;
+      this.contact = contact;
+      this.address = address;
+  }
+}
